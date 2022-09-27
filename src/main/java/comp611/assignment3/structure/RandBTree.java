188f2b7ba47dec51dc9b8f19631c5d9d4b2b2cbc
KMacnz <keanna.school@gmail.com>
2022-09-27 18:03:15 +1300

    init commit

---
+++ src/main/java/comp611/assignment3/structure/RandBTree.java
@@ -0,0 +1,33 @@
+package comp611.assignment3.structure;
+
+public class RandBTree {
+
+
+
+
+
+
+
+    public class redBlackNode {
+        public int key;
+        public int value;
+        public boolean isRed;
+        public redBlackNode left;
+        public redBlackNode right;
+        public redBlackNode parent;
+        public redBlackNode(int key, int value, boolean isRed, redBlackNode left, redBlackNode right, redBlackNode parent) {
+            this.key = key;
+            this.value = value;
+            this.isRed = isRed;
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+        }
+
+
+
+
+    }
+
+
+}
