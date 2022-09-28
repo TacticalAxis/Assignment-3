3bf9d234dd9f39b67f6d7a7d4dbf932abb97487a
Nathan <nathand1278@gmail.com>
2022-09-28 17:15:07 +1300

    Update BinarySearchTree.java

--- src/main/java/comp611/assignment3/structure/q1/BinarySearchTree.java
+++ src/main/java/comp611/assignment3/structure/q1/BinarySearchTree.java
@@ -4,60 +4,83 @@ import comp611.assignment3.structure.iface.SearchTree;
 
 public abstract class BinarySearchTree<E extends Comparable<E>> implements SearchTree<E> {
 
+    // root of the tree
     private Node<E> root;
 
+    // constructor
     public BinarySearchTree() {
         this.root = null;
     }
 
+    // get size of the tree
     @Override
     public int size() {
         int size = 0;
+        // iterate through the tree and count the number of nodes
         Node<E> current = root;
+
+        // if current children are null
         if(current.left == null && current.right == null) {
             return 0;
         }
+
+        // if current has a left child
         if(current.left != null) {
             size += size(current.left);
         }
+
+        // if current has a right child
         if(current.right != null) {
             size += size(current.right);
         }
+
         return size;
     }
 
     private int size(Node<E> node) {
         int size = 1;
+
+        // add the size of the left child
         if(node.left != null) {
             size += size(node.left);
         }
+
+        // add the size of the right child
         if(node.right != null) {
             size += size(node.right);
         }
+
         return size;
     }
 
     @Override
     public boolean add(E e) {
+        // if the tree is empty
         if (root == null) {
             root = new Node<>(e);
             return true;
         }
 
+        // iterate through the tree
         Node<E> current = root;
         while(true) {
-            if(e.compareTo(current.element) < 0) {
+            // check if the value is less than the current node
+            if(e.compareTo(current.value) < 0) {
                 if(current.left == null) {
+                    // add the value to the left
                     current.left = new Node<>(e);
                     break;
                 } else {
+                    // move to the left child
                     current = current.left;
                 }
-            } else if(e.compareTo(current.element) > 0) {
+            } else if(e.compareTo(current.value) > 0) {
                 if (current.right == null) {
+                    // add the value to the right
                     current.right = new Node<>(e);
                     break;
                 } else {
+                    // move to the right child
                     current = current.right;
                 }
             }
@@ -68,14 +91,17 @@ public abstract class BinarySearchTree<E extends Comparable<E>> implements Searc
 
     @Override
     public boolean remove(E value) {
+        // store ref to parent and current node
         Node<E> parent = null;
         Node<E> current = root;
 
+        // iterate through the tree
         while(current != null) {
-            if(value.compareTo(current.element) < 0) {
+            // check if the value is less than the current node
+            if(value.compareTo(current.value) < 0) {
                 parent = current;
                 current = current.left;
-            } else if(value.compareTo(current.element) > 0) {
+            } else if(value.compareTo(current.value) > 0) {
                 parent = current;
                 current = current.right;
             } else {
@@ -83,35 +109,42 @@ public abstract class BinarySearchTree<E extends Comparable<E>> implements Searc
             }
         }
 
+        // if the value is not found
         if(current == null) {
             return false;
         }
 
         if(current.left == null) {
+            // if the current node has no left child
             if(parent == null) {
                 root = current.right;
             } else {
-                if(value.compareTo(parent.element) < 0) {
+                // if the value is less than the parent
+                if(value.compareTo(parent.value) < 0) {
                     parent.left = current.right;
                 } else {
                     parent.right = current.right;
                 }
             }
         } else {
-            Node<E> parentOfRightMost = current;
+            // if the current node has a left child
             Node<E> rightMost = current.left;
+            Node<E> rightMostParent = current;
 
+            // iterate through the right most node
             while(rightMost.right != null) {
-                parentOfRightMost = rightMost;
+                rightMostParent = rightMost;
                 rightMost = rightMost.right;
             }
 
-            current.element = rightMost.element;
+            // replace the current node with the right most node
+            current.value = rightMost.value;
 
-            if(parentOfRightMost.right == rightMost) {
-                parentOfRightMost.right = rightMost.left;
+            // if the right most node has a left child
+            if(rightMostParent.right == rightMost) {
+                rightMostParent.right = rightMost.left;
             } else {
-                parentOfRightMost.left = rightMost.left;
+                rightMostParent.left = rightMost.left;
             }
         }
 
@@ -122,10 +155,11 @@ public abstract class BinarySearchTree<E extends Comparable<E>> implements Searc
     public boolean contains(E value) {
         Node<E> current = root;
 
+        // iterate through the tree
         while(current != null) {
-            if(value.compareTo(current.element) < 0) {
+            if(value.compareTo(current.value) < 0) {
                 current = current.left;
-            } else if(value.compareTo(current.element) > 0) {
+            } else if(value.compareTo(current.value) > 0) {
                 current = current.right;
             } else {
                 return true;
@@ -170,25 +204,25 @@ public abstract class BinarySearchTree<E extends Comparable<E>> implements Searc
     public abstract void hook(Node<E> node);
 
     private static class Node<E extends Comparable<E>> implements Comparable<E>{
-        //every node has e
-        public E element;
+        // every node has a value
+        public E value;
 
         //every node has a left and right node
         public Node<E> left;
         public Node<E> right;
 
         public Node(E element) {
-            this.element = element;
+            this.value = element;
         }
 
         @Override
         public int compareTo(E o) {
-            return element.compareTo(o);
+            return value.compareTo(o);
         }
 
         @Override
         public String toString() {
-            return "" + (left != null ? "[" + left + "]" : "") + element + (right != null ? "[" + right + "]" : "") + "";
+            return "" + (left != null ? "[" + left + "]" : "") + value + (right != null ? "[" + right + "]" : "") + "";
         }
     }
 }
