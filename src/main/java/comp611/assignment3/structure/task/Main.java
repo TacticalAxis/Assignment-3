114f552f82705fdb8b0fdecad7862120719f7f0a
Nathan <nathand1278@gmail.com>
2022-10-04 23:10:18 +1300

    keanna you are a bean

---
+++ src/main/java/comp611/assignment3/structure/task/Main.java
@@ -0,0 +1,12 @@
+package comp611.assignment3.structure.task;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree.main(args);
+        PersistentDynamicSet.main(args);
+        BalancedPersistentDynamicSet.main(args);
+
+        // for junit testing see src/test/java/comp611/assignment3/structure/task/testing/BinarySearchTreeTest.java
+    }
+}
